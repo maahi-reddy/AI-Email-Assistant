837ecfdc11df7a036d3a6b8a5ec6fb81037dc3c4
maahi-reddy <dachireddym@gmail.com>
2026-06-30 21:11:44 +0530

    fixed one api urls for production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -267,7 +267,7 @@ function App() {
   const sendEmail = async () => {
   try {
     // const response = await fetch("http://127.0.0.1:5000/send_email", {
-    const response = await fetch("https://ai-email-assistant-te5d.onrender.com/send_mail", {
+    const response = await fetch("https://ai-email-assistant-te5d.onrender.com/send_email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
